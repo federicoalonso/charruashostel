c9edece568de42e4bec9f5958da554c52c1938eb
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-08-22 21:46:07 -0300

    primer dashboar completo
    primer dashboar completo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
 
     <!-- overScroll Bars -->
     <link href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
 </head>
 
 <body>
@@ -35,7 +38,7 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+                    <a class="nav-link" href="#"><i class="fas fa-bars"></i></a>
                 </li>
             </ul>
         </nav>
@@ -45,9 +48,12 @@
                         Menu Lateral 
         **************************************** -->
         <div class="sidebar">
-            <div class="marca">
+            <div class="marca marca-gde">
                 <!-- <span>C</span>harrúas <span>H</span>ostel -->
                 <span>Charrúas</span> Hostel
+            </div>
+            <div class="marca marca-sm">
+                <span>C</span> H
             </div>
             <div class="sidebar-menu">
                 <center class="profile">
@@ -55,32 +61,54 @@
                     <p>Admin</p>
                 </center>
                 <li class="item">
-                    <a href="#" class="manu-btn">
-                        <i class="fa fa-desktop"><span>Escritorio</span></i>
+                    <a href="#" class="menu-btn">
+                        <i class="fas fa-desktop"></i><span>Escritorio</span>
                     </a>
                 </li>
-                <li class="item">
-                    <a href="#" class="manu-btn">
-                        <i class="fa fa-boxes"><span>Productos <i class="fa fa-chevron-down drop-down"></i></span></i>
+                <li class="item" id="productos">
+                    <a href="#productos" class="menu-btn">
+                        <i class="fa fa-boxes"></i><span>Productos <i class="fa fa-chevron-down drop-down"></i></span>
                     </a>
                     <div class="sub-menu">
                         <a href="#"><i class="fa fa-list-ul"></i><span>Resumen</span></a>
-                        <a href="#"><i class="fa fa-whmcs"></i><span>Configuración</span></a>
+                        <a href="#"><i class="fas fa-cogs"></i><span>Configuración</span></a>
                     </div>
                 </li>
             </div>
         </div>
 
 
-        <main class="py-4">
+        <!-- ***************************************
+                        Contenedor Principal
+        **************************************** -->
+        <main class="py-4 main-container">
             @yield('content')
+            <footer class="footer mt-5">
+                <strong>Copyright © 2020 <a href="http://kpacite.com">Kpacite.com</a>.</strong>
+                Derechos Reservados.
+                <div class="float-right d-none d-sm-inline-block">
+                    <b>Versión</b> 1.0.0
+                </div>
+            </footer>
         </main>
 
     </div>
 
+    <!-- JQuery -->
+    <script src="/plugins/jquery/jquery.min.js"></script>
+
     <!-- overScroll Bars -->
     <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <script src="/plugins/overlayScrollbars/js/OverlayScrollbars.min.js"></script>
+    
+    <!-- Script Página -->
+    <script>
+        $(document).ready(function(){
+            $('.nav-link').click(function(){
+                $('.wrapper').toggleClass('colapsado');
+            });
+        });
+    </script>
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-8 mx-auto">
+        <div class="col-12">
             <div class="card border-0">
+
                 <form action="{{ route('categorias.store') }}" method="POST">
                     @csrf
+                    <div class="card-header">
+                        Categorías
+                    </div>
                     <div class="card-body">
                         <input name="nombre" class="form-control border-0" type="text" placeholder="Ingrese la Categoría">
                     </div>
