325240459df218924c720ea0ff0433764c89a8b1
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-08-27 18:45:06 -0300

    agregando datatable
    agregando datatable

--- resources/js/components/CategoriasList.vue
+++ resources/js/components/CategoriasList.vue
@@ -1,25 +1,13 @@
 <template>
   <div class="col-12">
     <div class="card border-0">
+      <div class="card-header">
+        Tabla
+      </div>
+      <div class="card-body"></div>
+    </div>
+    <div class="card border-0 mt-4">
       <div v-for="categoria in categorias" v-text="categoria.nombre"></div>
-      <table id="table_id" class="display data-table-js">
-        <thead>
-          <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-          </tr>
-          <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   </div>
 </template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container pt-3">
     <div class="row">
         <div class="col-12 mb-4">
             <div class="card border-0">
